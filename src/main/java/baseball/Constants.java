86490b33403a566d98dd5f937fdb2bb962cd7b71
learner.k <heroinspace@naver.com>
2022-04-18 18:10:59 +0900

    (feat) Constants: 기능 요구 사항의 게임 관련 상수 선언

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Constants {
+    public static final int NUMBERS_LENGTH = 3;
+    public static final char NUMBER_START_CHAR = '1';
+    public static final char NUMBER_END_CHAR = '9';
+    public static final int NUMBER_START = NUMBER_START_CHAR - '0';
+    public static final int NUMBER_END = NUMBER_END_CHAR - '0';
+}
